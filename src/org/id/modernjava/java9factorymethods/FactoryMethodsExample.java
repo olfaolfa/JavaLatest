5f6cc9592812849e9e19124e6bc8677aa888b96a
olfaolfa <souissi.olfa91@gmail.com>
2020-04-09 13:16:19 +0200

    Java9: factory methods

---
+++ src/org/id/modernjava/java9factorymethods/FactoryMethodsExample.java
@@ -0,0 +1,30 @@
+package org.id.modernjava.java9factorymethods;
+
+
+import java.util.*;
+
+public class FactoryMethodsExample {
+    public static void main(String[] args) {
+        //How we used to create unmodifiable list pre Java 9
+        List<String> names = new ArrayList<>();
+        names.add("aa");
+        names.add("bb");
+        names.add("cc");
+        names = Collections.unmodifiableList(names);
+        System.out.println("names = " + names);
+        //names.add("ff"); UnsupportedOperationException
+
+        //factory methods of Java 9
+        List<String> words = List.of("bank", "money", "house");
+        System.out.println("words = " + words);
+        Set<String> set = Set.of("bank", "money", "house");
+        System.out.println("set = " + set);
+        Map<Integer, String> map = Map.of(1, "bank", 2, "house");
+        System.out.println("map = " + map);
+        //modify the list
+        //  words.add("salary"); UnsupportedOperationException
+        //set = Set.of("bank", "money", "house", "bank","money", "house"); java.lang.IllegalArgumentException: duplicate element: bank
+        //words.sort(Comparator.naturalOrder()); UnsupportedOperationException
+
+    }
+}
